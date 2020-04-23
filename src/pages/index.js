203c7c72fc22375e09083ea78b46adae907c9e20
winterandrea <andrecasero@gmail.com>
2020-04-23 12:47:41 +0200

    cache and public cleaned

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = ({ data }) => (
     <SEO title="Home" />
     <HeroText />
     <Hero img={data.img.childImageSharp.fluid} styleClass="default-hero" />
-    {/* <Product items={data.list} /> */}
+    <Product items={data.list} />
     <Products2 />
     <Contact />
   </Layout>
